c65248289eae8519c546b01a8506a1883b0174e0
Jack McCarthy <jackmc1112@gmail.com>
2024-10-01 10:41:59 -0400

    more library tests

--- tests/test_library.py
+++ tests/test_library.py
@@ -9,6 +9,7 @@ class TestLibrary(unittest.TestCase):
         self.library = library.Library()
         with open('tests_data/ebooks.txt', 'r') as source:
             self.books_data = json.loads(source.read())
+        
 
     def test_is_ebook_return_true(self):
         self.library.api.get_ebooks = Mock(return_value=self.books_data)
@@ -17,3 +18,24 @@ class TestLibrary(unittest.TestCase):
     def test_is_ebook_return_false(self):
         self.library.api.get_ebooks = Mock(return_value=self.books_data)
         self.assertFalse(self.library.is_ebook('War and Peace'))
+
+    def test_get_ebooks_count(self):
+        self.library.api.get_ebooks = Mock(return_value=self.books_data)
+        self.assertEqual(self.library.get_ebooks_count('Aprendendo Python'), 8)
+
+    def test_is_book_by_author_true(self):
+        self.library.api.get_ebooks = Mock(return_value=self.books_data)
+        self.assertTrue(self.library.is_book_by_author('M. Lutz','Aprendendo Python'))
+
+    def test_is_book_by_author_false(self):
+        self.library.api.get_ebooks = Mock(return_value=self.books_data)
+        self.assertFalse(self.library.is_book_by_author('Bob','Aprendendo Python'))
+
+    def test_get_languages_for_book(self):
+        self.library.api.get_ebooks = Mock(return_value=self.books_data)
+        self.assertEqual(self.library.get_languages_for_book('Aprendendo Python'),{'por'})
+
+
+    
+    
+    
