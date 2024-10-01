7a44858be4cd531c481dcafe588b7f1841c2ebad
Jack McCarthy <jackmc1112@gmail.com>
2024-10-01 11:32:39 -0400

    Fixed library tests to actually use mocking

--- tests/test_library.py
+++ tests/test_library.py
@@ -1,6 +1,8 @@
 import unittest
 from unittest.mock import Mock
+from unittest.mock import MagicMock
 from library import library
+from library import patron
 import json
 
 class TestLibrary(unittest.TestCase):
@@ -9,31 +11,34 @@ class TestLibrary(unittest.TestCase):
         self.library = library.Library()
         with open('tests_data/ebooks.txt', 'r') as source:
             self.books_data = json.loads(source.read())
+        self.library.api.get_ebooks = Mock(return_value=self.books_data)
+        self.library.db.
         
 
     def test_is_ebook_return_true(self):
-        self.library.api.get_ebooks = Mock(return_value=self.books_data)
         self.assertTrue(self.library.is_ebook('Aprendendo Python'))
 
     def test_is_ebook_return_false(self):
-        self.library.api.get_ebooks = Mock(return_value=self.books_data)
         self.assertFalse(self.library.is_ebook('War and Peace'))
 
     def test_get_ebooks_count(self):
-        self.library.api.get_ebooks = Mock(return_value=self.books_data)
         self.assertEqual(self.library.get_ebooks_count('Aprendendo Python'), 8)
 
     def test_is_book_by_author_true(self):
-        self.library.api.get_ebooks = Mock(return_value=self.books_data)
-        self.assertTrue(self.library.is_book_by_author('M. Lutz','Aprendendo Python'))
+        self.library.api.books_by_author = Mock(return_value={'Aprendendo Python','War and Peacse'})
+        self.assertTrue(self.library.is_book_by_author('Bob','Aprendendo Python'))
 
     def test_is_book_by_author_false(self):
-        self.library.api.get_ebooks = Mock(return_value=self.books_data)
-        self.assertFalse(self.library.is_book_by_author('Bob','Aprendendo Python'))
+        self.library.api.books_by_author = Mock(return_value={'Aprendendo Python','War and Peace'})
+        self.assertFalse(self.library.is_book_by_author('Bob','Old Yeller'))
 
     def test_get_languages_for_book(self):
-        self.library.api.get_ebooks = Mock(return_value=self.books_data)
-        self.assertEqual(self.library.get_languages_for_book('Aprendendo Python'),{'por'})
+        self.library.api.get_book_info = Mock(return_value=[{'language': 'a'}])
+        self.assertEqual(self.library.get_languages_for_book('Aprendendo Python'),{'a'})
+
+
+        
+
 
 
     
