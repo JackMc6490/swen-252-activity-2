ef295d7bc5e0cfc5d8278ad94bef7f5ae189db04
Jack McCarthy <jackmc1112@gmail.com>
2024-10-01 10:14:08 -0400

    Initial lib tests

---
+++ tests/test_library.py
@@ -0,0 +1,19 @@
+import unittest
+from unittest.mock import Mock
+from library import library
+import json
+
+class TestLibrary(unittest.TestCase):
+
+    def setUp(self):
+        self.library = library.Library()
+        with open('tests_data/ebooks.txt', 'r') as source:
+            self.books_data = json.loads(source.read())
+
+    def test_is_ebook_return_true(self):
+        self.library.api.get_ebooks = Mock(return_value=self.books_data)
+        self.assertTrue(self.library.is_ebook('Aprendendo Python'))
+
+    def test_is_ebook_return_false(self):
+        self.library.api.get_ebooks = Mock(return_value=self.books_data)
+        self.assertFalse(self.library.is_ebook('War and Peace'))
