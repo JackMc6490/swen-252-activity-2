d5a74cc81fcafa0bfc3fbb06e1046a02528e0a3d
Liam <lhw2837@rit.edu>
2024-10-01 10:47:02 -0400

    Progress?

--- tests/test_library.py
+++ tests/test_library.py
@@ -9,7 +9,6 @@ class TestLibrary(unittest.TestCase):
         self.library = library.Library()
         with open('tests_data/ebooks.txt', 'r') as source:
             self.books_data = json.loads(source.read())
-
     def test_is_ebook_return_true(self):
         self.library.api.get_ebooks = Mock(return_value=self.books_data)
         self.assertTrue(self.library.is_ebook('Aprendendo Python'))
