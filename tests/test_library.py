68d19e8fd1b349b9229b2913c500cff99119571a
Jack McCarthy <jackmc1112@gmail.com>
2024-10-06 13:57:16 -0400

    Comments for library and patron tests

--- tests/test_library.py
+++ tests/test_library.py
@@ -7,6 +7,7 @@ import json
 
 class TestLibrary(unittest.TestCase):
     
+    #loads test ebook data
     def setUp(self):
         self.library = library.Library()
         with open('tests_data/ebooks.txt', 'r') as source:
@@ -25,32 +26,46 @@ class TestLibrary(unittest.TestCase):
         self.library.api.get_ebooks = Mock(return_value=self.books_data)
         self.assertFalse(self.library.is_ebook('War and Peace'))
 
+
+    #Testing get_ebooks_count() w/ mock date
     def test_get_ebooks_count(self):
         self.library.api.get_ebooks = Mock(return_value=[{'title': 'Aprendendo Python', 'ebook_count': 3},{'title': 'Aprendendo Python', 'ebook_count': 4}])
         self.assertEqual(self.library.get_ebooks_count('Aprendendo Python'), 7)
 
+    #is_book_by_author() tests
+
+    #Testing is_book_by author when book is in mock data set
     def test_is_book_by_author_true(self):
         self.library.api.books_by_author = Mock(return_value={'Aprendendo Python','War and Peacse'})
         self.assertTrue(self.library.is_book_by_author('Bob','Aprendendo Python'))
-
+    
+    #Testing is_book_by author when book is not in mock data set
     def test_is_book_by_author_false(self):
         self.library.api.books_by_author = Mock(return_value={'Aprendendo Python','War and Peace'})
         self.assertFalse(self.library.is_book_by_author('Bob','Old Yeller'))
 
+    #Testing get_langauges_for_book() w/ mock data 
     def test_get_languages_for_book(self):
         self.library.api.get_book_info = Mock(return_value=[{'language': 'a'},{'title':'A cool book'},{'publish_year':1984}])
         self.assertEqual(self.library.get_languages_for_book('Aprendendo Python'),{'a'})
 
+
+    #is_patron_registered() tests
+
+    #Testing is_patron_registered() when retrieve_patron() returns the patron
     def test_is_patron_registered_true(self):
         testpatron = patron.Patron("Billy","Bob",12,"bb12")
         self.library.db.retrieve_patron = Mock(return_value=testpatron)
         self.assertTrue(self.library.is_patron_registered(testpatron))
 
+    #Testing is_patron_registered() when retrieve_patron() returns False
     def test_is_patron_registered_false(self):
         testpatron = patron.Patron("Billy","Bob",12,"bb12")
         self.library.db.retrieve_patron = Mock(return_value=False)
         self.assertFalse(self.library.is_patron_registered(testpatron))
 
+
+    #Testing borrow_book() properly calls all functions
     def test_borrow_book(self):
         testpatron = patron.Patron("Billy","Bob",12,"bb12")
         testpatron.add_borrowed_book = Mock()  
@@ -58,7 +73,8 @@ class TestLibrary(unittest.TestCase):
         self.library.borrow_book('Aprendendo Python',testpatron)
         self.library.db.update_patron.assert_called()
         testpatron.add_borrowed_book.assert_called()
-
+    
+    #Testing return_borrowed_book() properly calls all functions
     def test_return_borrowed_book(self):
         testpatron = patron.Patron("Billy","Bob",12,"bb12")
         testpatron.return_borrowed_book = Mock()  
@@ -67,20 +83,29 @@ class TestLibrary(unittest.TestCase):
         self.library.db.update_patron.assert_called()
         testpatron.return_borrowed_book.assert_called()
 
+    #is_borrowed_book() tests
+
+    #testing is_borrowed_book() when the book is borrowed
     def test_is_book_borrowed_true(self):
         testpatron = patron.Patron("Billy","Bob",12,"bb12")
         testpatron.get_borrowed_books = Mock(return_value={'aprendendo python','war and peace','horton hears a who'})
         self.assertTrue(self.library.is_book_borrowed('Aprendendo Python',testpatron))
 
+    #testing is_borrowed_book() when the book is not borrowed
     def test_is_book_borrowed_false(self):
         testpatron = patron.Patron("Billy","Bob",12,"bb12")
         testpatron.get_borrowed_books = Mock(return_value={'aprendendo python','war and peace','horton hears a who'})
         self.assertFalse(self.library.is_book_borrowed('Percy Jackson and the Lighting Thief',testpatron))
 
+
+    #register_patron tests
+
+    #testing register_patron when insert_patron returns the id
     def test_register_patron_success(self):
         self.library.db.insert_patron = Mock(return_value=12)
         self.assertEqual(self.library.register_patron("Billy","Bob",37,"12"),12)
 
+    #testing register_patron when insert_patron returns None
     def test_register_patron_failure(self):
         self.library.db.insert_patron = Mock(return_value=None)
         self.assertEqual(self.library.register_patron("Billy","Bob",37,"12"),None)
--- tests/test_patron.py
+++ tests/test_patron.py
@@ -6,10 +6,12 @@ import json
 
 class TestPatron(unittest.TestCase):
 
+    #Creates a test patron and adds one borrowed book
     def setUp(self):
         self.patron = patron.Patron("John","Doe",67,30)
         self.patron.add_borrowed_book('The Giving Tree')
 
+    #Test to confirm you can't have numbers in a patrons name
     def test_invalid_name(self):
         try:
             badpatron = patron.Patron("Bob123","Jones",21,137)
@@ -17,13 +19,14 @@ class TestPatron(unittest.TestCase):
         except:
             return True
 
+    #Testing borrowing a book
     def test_add_borrowed_book(self):
         self.patron.add_borrowed_book("War and Peace")
         if "War and Peace" in self.patron.get_borrowed_books():
             return True
         else:
             return False
-        
+    ##Testing borrowing a boom that has already been borrowed  
     def test_add_borrowed_book_already_borrowed(self):
         self.patron.add_borrowed_book('The Giving Tree')
         count = 0
@@ -32,7 +35,7 @@ class TestPatron(unittest.TestCase):
                 count+=1
         self.assertEqual(count,1)
 
-        
+    #Testing returning a book 
     def test_return_borrowed_book(self):
         self.patron.return_borrowed_book('The Giving Tree')
         if 'The Giving Tree' in self.patron.get_borrowed_books():
@@ -40,12 +43,17 @@ class TestPatron(unittest.TestCase):
         else:
             return True
         
+    #equals and not equals tests
+
     def test_equal(self):
         self.assertEqual(self.patron,self.patron)
 
     def test_not_equal(self):
         comparepatron = patron.Patron("Bob","Jones",21,137)
         self.assertNotEqual(self.patron,comparepatron)
+
+
+    #getter tests just for code coverage
 
     def test_get_fname(self):
         self.assertEqual(self.patron.get_fname(),"John")
