0c3d3900cdb5986c01a18706ccd59f2d9476fe1f
Swapnil K Acharjee <acharjee.swapnil@gmail.com>
2024-10-06 13:28:14 -0400

    finished mocking tests for db_interface

--- tests/test_library_db_interface.py
+++ tests/test_library_db_interface.py
@@ -6,6 +6,8 @@ Description: Testing module for the Library_DB class
 import unittest
 from unittest.mock import Mock, call
 from library import library_db_interface
+from library.patron import Patron
+from tinydb import TinyDB
 
 class TestLibrary_DB(unittest.TestCase):
     
@@ -18,6 +20,8 @@ class TestLibrary_DB(unittest.TestCase):
     }
     
     def setUp(self):
+        TinyDB.__init__ = Mock(return_value=None)
+        TinyDB.close = Mock()
         self.db_interface = library_db_interface.Library_DB()
         
     def tearDown(self):
@@ -70,6 +74,103 @@ class TestLibrary_DB(unittest.TestCase):
         # Test return value
         self.assertEqual(ret_val, None)
     
-    
+    def test_get_patron_count(self):
+        
+        # Mock Call
+        self.db_interface.db.all = Mock(return_value=[1,2,3,4])
+        
+        # Invoke
+        ret_val = self.db_interface.get_patron_count()
+        
+        # Evaluate
+        self.db_interface.db.all.assert_called()
+        self.assertEqual(4, ret_val)
+        
+    def test_get_all_patrons(self):
+        # Mock Call
+        data = [1,2,3,4]
+        self.db_interface.db.all = Mock(return_value=data)
+        
+        # Invoke
+        ret_val = self.db_interface.get_all_patrons()
+        
+        # Evaluate
+        self.db_interface.db.all.assert_called()
+        self.assertEqual(data, ret_val)
+        
+    def test_update_patron_none(self):
+        # Mock Call
+        self.db_interface.convert_patron_to_db_format = Mock()
+        
+        # Invoke
+        ret_val = self.db_interface.update_patron(None)
+        
+        # Evaluate
+        self.db_interface.convert_patron_to_db_format.assert_not_called()
+        self.assertEqual(ret_val, None)
+        
+    def test_update_patron(self):
+        mock_patron = Mock()
+        
+        # Mock Call
+        self.db_interface.convert_patron_to_db_format = Mock(return_value="fun")
+        mock_patron.get_memberID = Mock(return_value=12)
+        self.db_interface.db.update = Mock()
+        
+        # Invoke
+        self.db_interface.update_patron(mock_patron)
+        
+        # Evaluate
+        self.db_interface.db.update.assert_called()
+        mock_patron.get_memberID.assert_called()
+        self.db_interface.convert_patron_to_db_format.assert_called()
+        
+    def test_close_db(self):
+        # Mock Call
+        self.db_interface.db.close = Mock()
+        
+        #Invoke
+        self.db_interface.close_db()
+        
+        # Evaluate
+        self.db_interface.db.close.assert_called()
+        
+    def test_convert_patron_to_db_format(self):
+        mock_patron = Mock()
+        mock_patron.get_fname = Mock(return_value=self.test_data['fname'])
+        mock_patron.get_lname = Mock(return_value=self.test_data['lname'])
+        mock_patron.get_age = Mock(return_value=self.test_data['age'])
+        mock_patron.get_memberID = Mock(return_value=self.test_data['memberID'])
+        mock_patron.get_borrowed_books = Mock(return_value=self.test_data['borrowed_books'])
+        
+        # Invoke
+        ret_val = self.db_interface.convert_patron_to_db_format(mock_patron)
+        
+        # Evaluate
+        self.assertEqual(ret_val, self.test_data)
+        
+    def test_retrieve_patron(self):
+        # Mock Call
+        self.db_interface.db.search = Mock(return_value=[self.test_data])
+        
+        # Invoke
+        ret_val = self.db_interface.retrieve_patron(self.test_data['memberID'])
+        
+        # Evaluate
+        self.assertEqual(ret_val, Patron(
+            self.test_data['fname'],
+            self.test_data['lname'],
+            self.test_data['age'],
+            self.test_data['memberID']
+        ))
         
         
+    def test_retrieve_patron_not_in(self):
+        # Mock Call
+        self.db_interface.db.search = Mock(return_value=[])
+        
+        # Invoke
+        ret_val = self.db_interface.retrieve_patron(self.test_data['memberID'])
+        
+        # Evaluate
+        self.assertEqual(ret_val, None)
